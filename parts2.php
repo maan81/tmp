cca4feb2ce0d59c39e6b57beb5558f904443975c
Your Name <you@example.com>
2017-03-22 23:15:15 +0545

    removing serial in parts2

--- parts2.php
+++ parts2.php
@@ -10,7 +10,7 @@ $table_name = "3G_serial_numbers_parts";
 if ($mode == "search") {
 	$q = $_REQUEST['q'];
 	$isSearch = "&mode=search&q=".$q;
-	$showAll = '<a href="/serial/parts.php" class="addButton">Show All</a>';
+	$showAll = '<a href="/parts.php" class="addButton">Show All</a>';
 }
 
 
@@ -112,7 +112,7 @@ if(isset($_POST['submit']) || $mode == 'delete') {
 <br>
 <?php if(isset($prompt)) print $prompt?>
 <?php if($mode == 'add' || $mode == 'edit') :?>
-<div class="viewEditBox"> <a href="/serial/users.php" class="addButton">Cancel</a><br>
+<div class="viewEditBox"> <a href="/users.php" class="addButton">Cancel</a><br>
   <br>
   <form action="<?= $_SERVER['PHP_SELF']?>?mode=<?=$mode?>&entryID=<?=$entryID?>" method="post">
     <table cellpadding="3" cellspacing="3" border="0" width="100%">
@@ -314,7 +314,7 @@ while ($row = @mysqli_fetch_array($result1)) {
 ?>
   <table width="100%" border="0" cellspacing="3" cellpadding="2">
     <tr>
-      <td valign="middle" align="left"><a href="/serial/users.php" class="addButton">Close</a></td>
+      <td valign="middle" align="left"><a href="/users.php" class="addButton">Close</a></td>
       <td valign="middle" align="right"><a href="users.php?mode=edit&entryID=<?php echo $entryID; ?>" class="addButton">Edit</a></td>
     </tr>
     <tr>
