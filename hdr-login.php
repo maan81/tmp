adbd529b01357e838d8ed79aad7fff807f868e70
Your Name <you@example.com>
2017-03-22 23:27:59 +0545

    removing serial in hdr-login

--- hdr-login.php
+++ hdr-login.php
@@ -1,7 +1,7 @@
 <div class="hdrBG">
   <table cellpadding="4" cellspacing="2" border="0" class="marginCentered">
     <tr>
-      <td valign="center" class="logoTD"><a href="/serial/"><img src="/images/logo_home.png" height="50" /></a></td>
+      <td valign="center" class="logoTD"><a href="/"><img src="/images/logo_home.png" height="50" /></a></td>
       <td valign="center" class="logoTextTD"><h2 class="logoText"> Administration</h2></td>
     </tr>
     
